eb072f581257183e04f4a0229b0362f61596a913
Anyoneper <Anyoneper@163.com>
2025-03-10 15:58:56 +0800

    Add files via upload

---
+++ 3D/FVFG_3D.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+typedef unsigned long long bitmask_t;
+typedef unsigned long halfmask_t;
+
+#define ones(T,k) ((((T)1) << (k)) - 1) 
+#define getBit(X,i,k) (X>>(i-k+1)& ones(bitmask_t,k))  
+
+std::vector<uint16_t> CHM;
+std::vector<uint8_t> CSM;
+std::vector<uint16_t> HCM;
+std::vector<uint8_t> HSM;
+
+/*
+	en_FVFG, the virtual-filling based group encoding algorithm.
+	X: the first coordinate component
+	Y: the second coordinate component
+    Z: the third coordinate component
+	order: the number of orders
+	groupSize: group size
+*/
+void en_FVFG(bitmask_t& hcode, halfmask_t X, halfmask_t Y, halfmask_t Z, int order, int groupSize)
+{
+	halfmask_t groupX = 0, groupY = 0, groupZ = 0;
+	hcode = 0;
+	int pos = 0;
+
+	int groupNum = (order - 1) / groupSize + 1;
+	int startPos = groupNum * groupSize - 1;
+	int dimSize = 1 << groupSize;
+	unsigned state = (groupSize * groupNum - order) % 2 ? 5 : 0;
+
+	for (short i = groupNum - 1; i >= 0; i--)
+	{
+		groupX = getBit(X, startPos, groupSize);
+		groupY = getBit(Y, startPos, groupSize);
+		groupZ = getBit(Z, startPos, groupSize);
+		startPos -= groupSize;
+
+		pos = state * dimSize * dimSize * dimSize + groupX * dimSize * dimSize + groupY * dimSize + groupZ;
+		hcode = (hcode << (3 * groupSize)) | CHM[pos];
+		state = CSM[pos];
+	}
+}
+
+/*
+	de_FVFG, the virtual-filling based group decoding algorithm.
+	X: the first coordinate component
+	Y: the second coordinate component
+    Z: the third coordinate component
+	order: the number of orders
+	groupSize: group size
+*/
+void de_FVFG(bitmask_t hcode, halfmask_t& X, halfmask_t& Y, halfmask_t& Z, int order, int groupSize) {
+	X = Y = Z = 0;
+	unsigned bitZ = 0;
+	halfmask_t posKey = 0;
+	int groupNum = (order - 1) / groupSize + 1;
+	int dimSize = 1 << groupSize;
+	int startPos = 3 * groupNum * groupSize - 1;
+	halfmask_t mask = ones(halfmask_t, groupSize);
+	unsigned char state = (groupSize * groupNum - order) % 2 ? 5 : 0;
+	int pos = 0;
+
+	for (int i = groupNum - 1; i >= 0; i--) {
+		bitZ = getBit(hcode, startPos, 3 * groupSize);
+		startPos -= 3 * groupSize;
+		pos = state * dimSize * dimSize * dimSize + bitZ;
+		posKey = HCM[pos];
+
+		Z = (Z << groupSize) | (posKey & mask);
+		Y = (Y << groupSize) | ((posKey >> groupSize) & mask);
+		X = (X << groupSize) | ((posKey >> (2 * groupSize)) & mask);
+
+		// 更新状态为下一个组的状态
+		state = HSM[pos];
+	}
+}
+
+/*
+reading the state-views from files
+*/
+template <typename T>
+vector<T> readSVs(const string& filename) {
+	ifstream inFile(filename, ios::binary);
+	size_t size;
+	inFile.read(reinterpret_cast<char*>(&size), sizeof(size));
+	vector<T> array(size);
+	inFile.read(reinterpret_cast<char*>(array.data()), size * sizeof(T));
+	inFile.close();
+	return array;
+}
+
+
+int main()
+{
+	int n = 2;//the number of orders of the input data. This value should be between 1 to 32.
+	int g = 4; // group size. Do not change this value unless you have generated the state-views for the specific g. 
+	unsigned endVal = 1 << n;// we simply test the first endVal*endVal input data here.This value can be changed, however, it should be less than 1<<n.
+	bitmask_t hcode ;// Hilbert code
+	halfmask_t x, y, z;//coordinate
+
+	cout << "_________Reading the state views_____________" << endl;
+	CHM = readSVs<uint16_t>("CHM4.bin");
+	CSM = readSVs<uint8_t>("CSM4.bin");
+	HCM = readSVs<uint16_t>("HCM4.bin");
+	HSM = readSVs<uint8_t>("HSM4.bin");
+
+	cout << "_________Encoding_____________" << endl;
+	cout << endl << "Start test en_FVFG:" << endl;
+	for (z = 0; z < endVal; z++)
+	{
+		for (y = 0; y < endVal; y++)
+		{
+			for (x = 0; x < endVal; x++)
+			{
+				en_FVFG(hcode, x, y, z, n, g);
+				cout << "The Hilbert code of coordinate (" << x << "," << y << "," << z << ") is: " << hcode << endl;
+			}
+		}
+	}
+
+	cout << "_________Decoding_____________" << endl;
+	cout << endl << "Start test de_FVFG:" << endl;
+	for (hcode = 0; hcode < endVal * endVal * endVal; hcode++)
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+		de_FVFG(hcode, x, y, z, n, g);
+		cout << "The coordinate of Hilbert code " << hcode << " is: (" << x << "," << y << "," << z << ")" << endl;
+	}
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Efficient-Group-Based-Hilbert-Coding-and-Decoding-Algorithms
+#Efficient Group-Based Hilbert Coding and Decoding Algorithms
+
+The repository contains two folders: 2D and 3D.
+The 2D folder contains four files: CHM_8.bin, CSM_8.bin, HCM_8.bin, and HSM_8.bin, which correspond to the four computed 8-order state views, i.e., CHM, CSM, HCM, and HSM, respectively.
+The 3D folder contains four files: CHM_4.bin, CSM_4.bin, HCM_4.bin and HSM_4.bin, which correspond to the four calculated 4-order state views, i.e., CHM, CSM, HCM and HSM, respectively.
+
+Meanwhile Group_K.cpp provides the generation of the K-order state-views CHM_k, CSM_k, HCM_k, and HSM_k, and the order K can be modified by yourself. It can be opened and executed separately through Group_K.cpp.
+
+As an example for 2D: (the corresponding generation steps for 3D are the same).
+The steps to open and execute FVFG.cpp are as follows:
+1, First create a new project; 
+2. Add the FVFG.cpp file to that project; 
+3. Open the local folder where the FVFG.cpp file is stored; 
+4. Copy the four files CHM_8.bin, CSM_8.bin, HCM_8.bin and HSM_8.bin to the local folder where the FVFG.cpp file is stored; 
+5、Execute the FVFG.cpp file in the compiler, such as visual studio 2022, Visual Studio Code, and so on;
+
+In the main function, we first load four 8th order state views from four files (same for 3D) and then call the following four algorithms respectively:
+en_FVFG:  the virtual-filling based group encoding algorithm
+de_FVFG:  the virtual-filling based group decoding algorithm
+
+Note that do not change the group size parameter g in the main function unless you have already generated a state view for the specific g.
